9a117e5534fcd7566633b64a8c5c21fb554fec26
swiss97501-max <swiss97501@gmail.com>
2026-06-03 15:22:03 +0700

    Update graph.js

--- Knowledge-Vault/memory_engine/graph.js
+++ Knowledge-Vault/memory_engine/graph.js
@@ -1,1 +1,20 @@
+export function buildGraph(claims) {
 
+    const nodes = [];
+    const edges = [];
+
+    claims.forEach(claim => {
+
+        nodes.push({
+            id: claim.id,
+            label: claim.original
+        });
+
+    });
+
+    return {
+        nodes,
+        edges
+    };
+
+}
